7a6cd745643705dc92196a82afaaf53b634c1383
oddstab <oddstab@gmail.com>
2020-05-19 02:46:11 +0800

    峰華企業官網-圖片輪播圖完成

--- 練習/切版/峰華企業官網/index.js
+++ 練習/切版/峰華企業官網/index.js
@@ -1,2 +1,23 @@
 const glide = new Glide(".glide");
+const captionsEl = document.querySelectorAll(".slide-caption");
+
+glide.on(["mount.after", "run.after"], () => {
+    console.log(captionsEl);
+    const caption = captionsEl[glide.index];
+    anime({
+        targets: caption.children,
+        opacity: [0, 1],
+        duration: 200,
+        easing: "linear",
+        delay: anime.stagger(300, { start: 300 }),
+        translateY: [anime.stagger([40, 10]), 0]
+    });
+})
+
+glide.on("run.before", () => {
+    document.querySelectorAll(".slide-caption > *").forEach((el) => {
+        el.style.opacity = 0;
+    });
+});
+
 glide.mount();
